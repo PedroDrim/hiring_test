90342f9ccd2a6822edf752586266c91ad87275bd
pedro <pedrohenriquedrim@gmail.com>
2025-04-11 02:07:19 -0300

    created route for scores

--- backend/controllers/memoryController.js
+++ backend/controllers/memoryController.js
@@ -1,3 +1,4 @@
+const req = require('express/lib/request');
 const Save = require('../models/save');
 
 exports.saveGameData = async (req, res) => {
@@ -27,3 +28,47 @@ exports.saveGameData = async (req, res) => {
         res.status(500).json({ message: 'Error saving game data', error });
     }
 };
+
+exports.getHighScoreData = async (req, res) => {
+    try {
+        const difficulty = req.params.difficulty
+
+        if (difficulty == null || difficulty == "" || typeof difficulty !== 'string') {
+            return res.status(400).json({ message: 'Invalid field type' });
+        }
+
+        const query = await Save.aggregate([
+            {$match: {
+                difficulty: {$eq: difficulty}, 
+                completed: {$gt: 0}
+            }},
+
+            {$lookup: {
+                from: "users",
+                localField: "userID",
+                foreignField: "_id",
+                as: "userData"
+            }},
+
+            {$unwind: "$userData"}, 
+
+            {$project: {
+                _id: 0,
+                username: "$userData.username",
+                failed: 1,
+                completed: 1,
+                timeTaken: 1,
+            }},
+
+            {$sort: {
+                completed: -1,
+                timeTaken: 1
+            }}
+        ])
+        
+        res.status(201).json(query);
+    } catch (error) {
+        console.error('Error saving game data:', error);
+        res.status(500).json({ message: 'Error saving game data', error });
+    }
+}
--- backend/routes/memoryRoutes.js
+++ backend/routes/memoryRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { saveGameData } = require('../controllers/memoryController');
+const { saveGameData, getHighScoreData } = require('../controllers/memoryController');
 const router = express.Router();
 
 // Route to save game data
 router.post('/save', saveGameData);
+router.get('/highScore/:difficulty', getHighScoreData);
 
 module.exports = router;
